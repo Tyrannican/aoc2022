be96e50b275fd482823b0549bb012c78532dc506
Graham Keenan <graham.keenan@outlook.com>
2022-12-15 20:50:18 +0000

    Day 10 Part 1 Solution

--- src/day10/mod.rs
+++ src/day10/mod.rs
@@ -2,25 +2,74 @@
 
 use crate::utils::*;
 
-pub struct Solution {
+#[derive(Debug, PartialEq)]
+enum Operation {
+    Noop,
+    Addx(i32)
+}
 
+pub struct Solution {
+    cycle: i32,
+    register: i32,
+    operations: Vec<Operation>
 }
 
 impl Solution {
     pub fn new() -> Self {
-        let mut sol = Self {};
+        let mut sol = Self {
+            cycle: 1,
+            register: 1,
+            operations: vec![]
+        };
         sol.process_input("day10/input.txt");
         sol
+    }
+
+    fn cycle_check(&self, register_values: &mut Vec<i32>) {
+        if self.cycle == 20
+            || self.cycle == 60
+            || self.cycle == 100
+            || self.cycle == 140
+            || self.cycle == 180
+            || self.cycle == 220
+        {
+            register_values.push(self.cycle * self.register);
+        }
     }
 }
 
 impl Solve for Solution {
     fn process_input(&mut self, path: &str) {
-        let _raw = read_file(path);
+        let raw = read_file(path);
+
+        self.operations = raw.split('\n')
+            .map(|line| {
+                let split = line.split(' ').collect::<Vec<&str>>();
+                if split.len() == 1 { return Operation::Noop }
+
+                Operation::Addx(split[1].parse::<i32>().unwrap())
+            })
+            .collect();
     }
 
     fn part1(&mut self) {
-        
+        let mut register_values: Vec<i32> = vec![];
+        for op in self.operations.iter() {
+            match op {
+                Operation::Addx(value) => {
+                    for _ in 0..2 {
+                        self.cycle_check(&mut register_values);
+                        self.cycle += 1;
+                    }
+                    self.register += value;
+                }
+                Operation::Noop => {
+                    self.cycle_check(&mut register_values);
+                    self.cycle += 1
+                }
+            }
+        }
+        println!("Part 1: {}", register_values.iter().sum::<i32>());
     }
 
     fn part2(&mut self) {
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
 mod utils;
-mod day09;
+mod day10;
 
 use utils::Solve;
 
 fn main() {
-    let mut sol = day09::Solution::new();
+    let mut sol = day10::Solution::new();
     sol.part1();
     sol.part2();
 }
